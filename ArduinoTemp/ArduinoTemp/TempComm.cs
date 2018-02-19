f5af37ba0e19ea28007dda249b5fd6a9100fa809
bman46 <brendanmcs7@gmail.com>
2018-02-19 13:46:14 -0500

    Visual Studio Files

---
+++ ArduinoTemp/ArduinoTemp/TempComm.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+using OpenHardwareMonitor.Hardware;
+
+namespace ArduinoTemp
+{
+    public partial class TempComm : ServiceBase
+    {
+        public TempComm()
+        {
+            InitializeComponent();
+
+        }   
+
+        protected override void OnStart(string[] args)
+        {
+            // Source: https://docs.microsoft.com/en-us/dotnet/framework/windows-services/walkthrough-creating-a-windows-service-application-in-the-component-designer#BK_CreateProject
+            // Set up a timer to trigger every minute.  
+            if (!serialPort1.IsOpen)
+            {
+                try
+                {
+                    serialPort1.Open();
+
+                    
+                }
+                catch
+                {
+                    // serialPort1.Write(outputTemp);
+                    // If not connected or other error, wait 1 minute:
+                    System.Threading.Thread.Sleep(120000);
+                }
+            }
+
+            System.Timers.Timer timer = new System.Timers.Timer();
+            timer.Interval = 5000; // 60 seconds  
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(this.OnTimer);
+            timer.Start();
+        }
+
+        protected override void OnStop()
+        {
+            serialPort1.Close();
+        }
+
+        public void OnTimer(object sender, System.Timers.ElapsedEventArgs args)
+        {
+
+                try
+                {
+                    // Change CPU to true to read CPU temps, will need to modify code and elevate permissions. Currently Set to GPU
+                    Computer computer = new Computer() { CPUEnabled = false, GPUEnabled = true };
+                    computer.Open();
+
+                    foreach (IHardware hardware in computer.Hardware)
+                    {
+                        hardware.Update();
+
+                        foreach (ISensor sensor in hardware.Sensors)
+                        {
+                            // Celsius is default unit
+                            if (sensor.SensorType == SensorType.Temperature)
+                            {
+                                string temps = Convert.ToString(sensor.Value);
+                                string.Format("{0:###}", temps);
+                                serialPort1.Write(temps);
+                                // Console.WriteLine("{0}: {1}°F", sensor.Name, sensor.Value*1.8+32);
+                                // Modified from a Open Hardware Monitor example
+                            }
+
+                        }
+
+                    }
+                }
+                catch
+                {
+                    // serialPort1.Write(outputTemp);
+                    // If not connected or other error, wait 1 minute:
+                    System.Threading.Thread.Sleep(120000);
+                }
+         
+
+        }
+    }
+}
---
+++ TempDisplay.ino
@@ -0,0 +1,49 @@
+/*************************************************************************
+* Demo sketch of Arduino Text Display Library for Multiple LCDs
+* Distributed under GPL v2.0
+* Copyright (c) 2013 Stanley Huang <stanleyhuangyc@live.com>
+* All rights reserved.
+*************************************************************************/
+
+#include <Arduino.h>
+#include <Wire.h>
+#include <MultiLCD.h>
+
+
+LCD_SSD1306 lcd; /* for SSD1306 OLED module */
+//LCD_PCD8544 lcd; /* for LCD4884 shield or Nokia 5100 screen module */
+//LCD_1602 lcd; /* for LCD1602 shield */
+//LCD_ZTOLED lcd; /* for ZT OLED module */
+//LCD_ILI9325D lcd; /* for Itead 2.8" TFT shield */
+
+void setup()
+{
+  Serial.begin(9600);
+  delay(1000);
+  lcd.begin();
+}
+
+void loop()
+{
+   if (Serial.available() > 0) {
+       // read the incoming byte:
+       String incomingByte = Serial.readString();
+       // read temp
+          lcd.setCursor(35, 0);
+          lcd.setFont(FONT_SIZE_SMALL);
+          lcd.print("GPU Temp:");
+          
+          lcd.setCursor(45, 2.5);
+          lcd.setFont(FONT_SIZE_XLARGE);
+          lcd.print(incomingByte);
+
+          lcd.setCursor(70, 2.6);
+          lcd.setFont(FONT_SIZE_SMALL);
+          lcd.print("C");
+          delay(4000);
+          lcd.clear();
+          
+        }
+ }
+
+
